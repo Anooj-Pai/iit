40971aa51790cbb9b8b60be2a2b5ed74f742f318
Anooj Pai <anooj8000@gmail.com>
2021-11-08 17:06:52 -0500

    Quiz2
    Updating files for quiz 2

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # iit
-iit website
+# Quiz2
+
+For question di) I created a new menu on in the header and also used jQuery to pull the lab data from the json file and create a drop down for it.
+
+dii) Upon loading the page, it will create a dialog window that has the instructions for the lab in it. The window is resizable and moveable as the instructions expect.
+
+diii) For my projects page I created an accordion for each of the labs and wrote a little summary for each one.
+
+div) I added a simple button and draggable block on my home page just for fun and to play around with the widgets.
---
+++ iit/Labs/labs.js
@@ -0,0 +1,40 @@
+{
+    "Labs": [
+        {
+            "Name": "Lab 2",
+            "Desc": "Resume",
+            "Link": "../Labs/Lab2/index.html",
+            "instructions": "../Labs/Lab2/Lab2-Instructions.html"
+        },
+        {
+            "Name": "Lab 3",
+            "Desc": "Website",
+            "Link": "../Labs/Lab3/index.html",
+            "instructions": ""
+        },
+        {
+            "Name": "Lab 4",
+            "Desc": "Atom",
+            "Link": "../Labs/Lab4-atom/Atomfeed.xml",
+            "instructions": ""
+        },
+        {
+            "Name": "Lab 4",
+            "Desc": "RSS",
+            "Link": "../Labs/Lab4-rss/RSSfeed.xml",
+            "instructions": ""
+        },
+        {
+            "Name": "Lab 5",
+            "Desc": "JavaScript",
+            "Link": "../Labs/Lab5/index.html",
+            "instructions": "../Labs/Lab5/Lab5-Instructions.html"
+        },
+        {
+            "Name": "Lab 6",
+            "Desc": "JQuery",
+            "Link": "../Labs/Lab6/index.html",
+            "instructions": ""
+        }
+    ]
+}
